3005125768ad1a9ab75ab7fcbd637f797a22ec95
sahin <sahin@FALLIEN>
2023-05-03 22:58:06 +0400

    Add project files.

---
+++ may3task/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace oop;
+using may3task;
+using System.Drawing;
+class Program 
+{
+    static void Main()
+    {
+        foreach(Status st in Enum.GetValues(typeof(Status)))
+        {
+          
+            Console.WriteLine(
+                st
+                + "-->"
+                + st.GetDisplayName());
+            
+
+        }
+        Console.ReadLine();
+      
+    }
+}
+
+
---
+++ may3task/ennum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Reflection;
+
+namespace may3task
+{
+
+
+    public enum Status : byte
+    {
+        None = 0,
+
+
+        [Display(Name = "Aktif Kullanıcıları Listeler")]
+        Active = 1,
+
+
+        [Display(Name = "Pasif Kullanıcıları Listeler")]
+        Passive,
+
+
+        [Display(Name = "Silinmiş Kullanıcıları Listeler")]
+        Deleted,
+
+
+        [Display(Name = "Kaydını Dondurmuş Kullanıcıları Listeler")]
+        Holiday
+    }
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var displayAttribute = enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>();
+
+            if (displayAttribute != null)
+            {
+                return displayAttribute.GetName();
+            }
+            else
+            {
+                return enumValue.ToString();
+            }
+        }
+    }
+
+}
